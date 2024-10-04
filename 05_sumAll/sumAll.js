ce4a27179b47d2fba113d2911682ff56ec292588
Anam <121228616+hiighyears@users.noreply.github.com.>
2024-10-05 04:07:49 +0530

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum,lastNum) {
+    let totalSum = 0;
+    if(firstNum>lastNum){
+        let middleMan = 0;
+        middleMan = firstNum;
+        firstNum = lastNum;
+        lastNum = middleMan;
+    }
+    if(firstNum<0 || lastNum<0 || firstNum%1!=0 || lastNum%1!=0 || !Number.isInteger(firstNum) ||!Number.isInteger(lastNum)){
+        return totalSum="ERROR";
+    }
+    for(let i = firstNum;i<=lastNum;i++){
+        totalSum += i;
+    }return totalSum;
 };
 
 // Do not edit below this line
