a7689a058f415ae7388cbebdaed5a6c2addfb83b
Anam <121228616+hiighyears@users.noreply.github.com.>
2024-10-05 02:56:02 +0530

    04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    // let newArray= [];
+    for(let j = 1;j<arguments.length;j++) {
+    for(let i = 0;i<array.length;i++){
+        for(let k = 0;k<array.length;k++){
+
+        if (array[i]===arguments[j])
+            // newArray.push(array[i]);
+        array.splice(i,1);}
+    }
+    // array = newArray ;
+}
+return array;
 };
 
 // Do not edit below this line
