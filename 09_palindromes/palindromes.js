871c75c551cc0b37baa38e778c8cc8ad0551868b
Anam <121228616+hiighyears@users.noreply.github.com.>
2025-04-23 20:54:50 +0530

    done with some gelp

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (word) {
+    const lowword = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const muck = lowword.split('').reduce((tot, currval, currindex) => {
+      if (currindex >= Math.floor(lowword.length / 2)) {
+        return tot;
+      }
+      return currval === lowword[lowword.length - 1 - currindex] ? tot : false;
+    }, true);
+    return muck;
+  };
 // Do not edit below this line
 module.exports = palindromes;
