11f64443efddc12cfb9b557673c5d3c933771d99
Anam <121228616+hiighyears@users.noreply.github.com.>
2025-04-23 11:10:40 +0530

    solved array probs

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  return a.reduce((tot,curr)=>tot + curr,0);};
+
+const multiply = function(a) {
+  return a.reduce((tot,curr)=>tot * curr);
 };
 
-const multiply = function() {
-
+const power = function(a,b) {
+  let tot = 1;
+  for(let i=0;i<b;i++){
+ tot*=a;
+  }
+	return tot;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a==0 || a==1){
+    return 1;}
+  else{
+  let tot=1;
+  for(let i=1;i<=a;i++){
+    tot*=i;} 
+    return tot;}
 };
 
 // Do not edit below this line
